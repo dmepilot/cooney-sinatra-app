3980c593d4c0843efa2ed05817b9311e2b8e9ac8
dmepilot <dmepilot@yahoo.com>
2021-06-01 16:26:09 -0600

    styling and user functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,36 @@ class UsersController < ApplicationController
     end
     
     post '/manager' do
-        user=User.find_by(:username => params[:username])
-        if user && user.authenticate(params[:password])
-            session[:user_id]=user.id
+        @user=User.find_by(:username => params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id]=@user.id
             flash[:success]="Successfully signed in as #{user.username}."
             redirect to("/postings")
 
         else
             flash[:error]=" Invalid Login. Please try again." 
             redirect to("/manager")
+        end
+    end
+
+    get '/users/new' do
+        erb :'/users/new.html'
+    end
+
+    post '/users/new' do
+        unless User.find_by(:username => params[:username]) 
+          user=User.new(:username => params[:username], :password => params[:password])
+          if user.save 
+            session[:user_id]=user.id
+            flash[:success]="Successfully signed in as #{user.username}."
+            redirect to("/postings")
+          else
+            flash[:incomplete]="Please fill in all fields" 
+            redirect to "/signup"
+         end
+        else
+          flash[:taken]="Username already in use" 
+          redirect to "/users/new"
         end
     end
 
@@ -27,6 +48,7 @@ class UsersController < ApplicationController
         @user = User.find_by_slug(params[:slug])
         @user.update(params[:user])
         @user.save
+        flash[:updated]="Profile successfully updated."
         redirect("/postings")
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
+    validates :username, presence: true 
+
 
     def slug
         self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
